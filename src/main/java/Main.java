fca8e42ebb24d43b1fd77dc9a364d48a07b474b0
arberyoda <a.heqimi@bluewin.ch>
2018-12-31 20:11:06 +0100

    First test class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,30 @@
-public class Main
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application
 {
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        Button btn = new Button();
+        btn.setText("Klick me");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 600, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Title");
+        primaryStage.show();
+    }
 }
